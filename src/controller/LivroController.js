2b1ffdd0150e1489c46d5d257832a83c5bc0d871
Fri5Day <victoramaral951@outlook.com>
2024-03-28 18:09:02 -0300

    feat: funcionalidade alugar/devolver livro

--- src/controller/LivroController.js
+++ src/controller/LivroController.js
@@ -3,7 +3,7 @@ import { prisma } from "../database/prisma.js";
 
 export const findAllLivro = async (req, res) => {
     try {
-        const livro = await prisma.livro.findMany();
+        const livro = await prisma.livro.findMany({include: {categoria: true} });
         return res.json(livro);
     } catch (error) {
         console.error(error);
@@ -36,9 +36,11 @@ export const createLivro = async (req, res) => {
     const categoriaInt = parseInt(categoriaId, 10);
 
     const livro = await prisma.livro.create({
-        data: { titulo, autor, descricao, categoriaId: categoriaInt, 
-            anoPublicacao: anoPublicacaoInt, copias: copiasInt, 
-            copiasDisponiveis: copiasDisponiveisInt },
+        data: {
+            titulo, autor, descricao, categoriaId: categoriaInt,
+            anoPublicacao: anoPublicacaoInt, copias: copiasInt,
+            copiasDisponiveis: copiasDisponiveisInt
+        },
     });
 
     return res.json(livro);
@@ -59,6 +61,62 @@ export const updateLivro = async (req, res) => {
         res.status(500).json({ error });
     }
 };
+
+export const alugarLivro = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { quantidade } = req.body;
+
+        if (quantidade <= 0) {
+            return res.status(400).json({ message: "A quantidade não pode ser igual ou menor que zero." })
+        }
+
+        const livro = await prisma.livro.findUnique({
+            where: { id: Number(id) },
+        })
+
+        if (livro.copiasDisponiveis >= quantidade) {
+            const updateLivro = await prisma.livro.update({
+                where: { id: Number(id) },
+                data: { copiasDisponiveis: livro.copiasDisponiveis - quantidade }
+            });
+
+            return res.status(200).json({message: "Quantidade atualizada com sucesso."})
+        }
+        return res.status(400).json({message: "A quantidade não pode ser maior que o número de cópias disponiveis."})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error });
+    }
+}
+
+export const devolverLivro = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { quantidade } = req.body;
+
+        if (quantidade <= 0) {
+            return res.status(400).json({ message: "A quantidade não pode ser igual ou menor que zero." })
+        }
+
+        const livro = await prisma.livro.findUnique({
+            where: { id: Number(id) },
+        })
+
+        if (livro.copias >= livro.copiasDisponiveis + Number(quantidade)) {
+            const updateLivro = await prisma.livro.update({
+                where: { id: Number(id) },
+                data: { copiasDisponiveis: livro.copiasDisponiveis + Number(quantidade) }
+            });
+
+            return res.status(200).json({message: "Quantidade atualizada com sucesso."})
+        }
+        return res.status(400).json({message: "As cópias disponiveis não podem ser maior que o número de cópias."})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error });
+    }
+}
 
 export const deleteLivro = async (req, res) => {
     try {
--- src/routers/livro.routes.js
+++ src/routers/livro.routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 const router = express();
-import { findAllLivro, findByIdLivro, createLivro, updateLivro, deleteLivro } from "../controller/LivroController.js"
+import { findAllLivro, findByIdLivro, createLivro, updateLivro, deleteLivro, alugarLivro, devolverLivro } from "../controller/LivroController.js"
 
 router.get("/livro", findAllLivro);
 router.get("/livro/:id", findByIdLivro); 
 router.post("/livro", createLivro);
 router.put("/livro/:id", updateLivro);
+router.put("/livro/:id/alugar", alugarLivro);
+router.put("/livro/:id/devolver", devolverLivro);
 router.delete("/livro/:id", deleteLivro);
 
 export default router;
